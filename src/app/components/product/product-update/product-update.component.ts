8478cf94ba127540532524b3ca5ef0aeee7802e6
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-06-23 11:49:19 -0300

    Atualização do produto funcionando OK.

--- src/app/components/product/product-update/product-update.component.ts
+++ src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../products/products.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product
 
+  constructor(private productService: ProductsService, private router: Router,
+    private route: ActivatedRoute) { }
+
+  // Para preencher os campos quando entrar na tela de update. (OO)
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
 }
